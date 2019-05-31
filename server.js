cdf9e598828ad7922947d06487675d01a05e1180
JoeLong2019 <50843137+JoeLong2019@users.noreply.github.com>
2019-05-31 16:06:22 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, 'TicTacToe')));
+
+// app.get('/', (req, res) => {
+//     res.send('<h1>Tic Tac Toe!!</h1>');
+// });
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
